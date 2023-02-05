cfd465e28795eb71057183f5a73dc4d3dcb4d5fe
Mahdi <mahdi_hidden2000@yahoo.com>
2023-02-05 23:01:27 +1100

    Added the input

--- madlib.py
+++ madlib.py
@@ -1,6 +1,8 @@
 #this is a concationator method
 
-def join_sentences(sentence_1, sentence_2):
-    print(f"Sentence_1 joined to Sentence_2 : {sentence_1 + sentence_2}")
+def join_sentences():
+    sentence_1 = input("Please Write a sentence: ")
+    sentence_2 = input("Please Write another sentence: ")
+    print(f"Sentence_1 joined to Sentence_2 : {sentence_1} {sentence_2}")
 
-join_sentences("I love you", "very much")
+join_sentences()
