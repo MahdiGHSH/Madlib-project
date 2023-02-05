25ca978c493844e254c0988ed03ae39ba7d8f17e
Mahdi <mahdi_hidden2000@yahoo.com>
2023-02-05 22:44:09 +1100

    First commit

---
+++ madlib.py
@@ -0,0 +1,6 @@
+#this is a concationator method
+
+def join_sentences(sentence_1, sentence_2):
+    print(f"Sentence_1 joined to Sentence_2 : {sentence_1 + sentence_2}")
+
+join_sentences("I love you", "very much")
